7721a07614c29bdec6fb71298dcf027b398deac3
Hallelujah <hery@rails-royce.org>
2012-01-30 13:06:13 +0100

    Add loading of documents from output

--- lib/sge/qstat/job.rb
+++ lib/sge/qstat/job.rb
@@ -11,6 +11,46 @@ module SGE
         :JB_submission_time => :submission_time,
         :state => :state
       )
+
+      def self.load_documents(opts,&block)
+        documents = []
+
+        # Make a fifo to dump file
+        fifo = opts[:file] || SGE::Utils.mkfifo(opts[:tmp_dir])
+
+        # Compute command
+        cmd = command(opts.merge(:fifo => fifo))
+
+        # Execute this command
+        SGE::Utils.execute(cmd)
+
+        # We need a XML parser
+
+        # Read file from fifo and instantiate jobs
+        SGE::Utils.read_file(fifo, true) do |file|
+          # Parse the file
+          doc = SGE::QStat::Parser.parse(file)
+          # Loop through the jobs found
+          doc.jobs.each do |j|
+            if block_given?
+              documents << yield(self.from_document(j))
+            else
+              documents << self.from_document(j)
+            end
+          end
+        end
+        documents
+      end
+
+      def self.command(opts)
+        if opts[:cmd]
+          opts[:cmd]
+        else
+          file = opts[:sample] || File.expand_path('../../../../spec/data/qstat.sge',__FILE__)
+          "cat #{file} > #{opts[:fifo]} &"
+        end
+      end
+
     end
   end
 end
